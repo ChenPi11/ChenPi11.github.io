63e1fcfdab31f0543b9d5f9d81433910636b3020
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2025-01-25 23:11:15 +0800

    Build wwwroot.

---
+++ wwwroot/posts/applets/2025-01-25-0/1.js
@@ -0,0 +1,1 @@
+"use strict";window.addEventListener("load",(function(){let termElement=document.getElementById("80x25");const term=termElement.terminal;for(let i=0;i<25;i++){term.writeln("00000000000000000000000000000000000000000000000000000000000000000000000000000000")}}));
