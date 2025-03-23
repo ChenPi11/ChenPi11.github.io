ec99ae45b6dd40c83e47699661cc3141767ccace
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2025-03-23 21:26:23 +0800

    Rebuild.

---
+++ wwwroot/lib/mathjax.js
@@ -0,0 +1,1 @@
+"use strict";window.MathJax={tex:{inlineMath:[["$","$"],["\\(","\\)"]],displayMath:[["$$","$$"],["\\[","\\]"]],processEscapes:true,processEnvironments:true},options:{processHtmlClass:"arithmatex"}};
---
+++ wwwroot/posts/res/2025-03-23-0/hello-world.js
@@ -0,0 +1,1 @@
+"use strict";import{getTerminal,fprintf}from"/lib/terminal.js";$((function(){const _=getTerminal("hello-world");const term=_.terminal;const stdin=_.stdin;fprintf(term,"Hello World!\n")}));
